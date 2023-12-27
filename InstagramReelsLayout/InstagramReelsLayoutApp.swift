57df7cf1445bbd26cb9fbc25f31b4962c1bfb3ba
IvanL <antila3567@gmail.com>
2023-12-27 15:02:55 +0200

    Initial Commit

---
+++ InstagramReelsLayout/InstagramReelsLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramReelsLayoutApp.swift
+//  InstagramReelsLayout
+//
+//  Created by Иван Легенький on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramReelsLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
